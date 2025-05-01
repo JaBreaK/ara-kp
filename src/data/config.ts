b379b17b7f5d3c71a0a968ae9907d544eed9c68a
ahhel <rahelazzahra2004@gmail.com>
2025-05-01 14:26:39 +0700

    Add files via upload

---
+++ src/data/config.ts
@@ -0,0 +1,58 @@
+// Global variables
+export const siteLang = "id-ID";
+export const siteCurrency = "INA";
+export const siteDomain = "astropie.com";
+export const siteName = "ARA";
+export const siteSlogan = "The best pies in the galaxy!";
+export const themeColor = "#FF0000";
+
+export const phone = {
+	href: "https://api.whatsapp.com/send/?phone=6285778703278&text&type=phone_number&app_absent=0",
+	label: "0857-7870-3278",
+};
+
+export const email = {
+	href: "mailto:info@astropie.com",
+	label: "info@astropie.com",
+};
+
+export const address = {
+	street: "Jl. Buyut Sanun No.38, RT.004/RW.005 Kel. Cikokol, Kec. Tangerang",
+	city: "Kota Tangerang",
+	zip: "15117",
+	state: "Banten",
+	country: "Indonesia",
+};
+
+export const socialMedia = {
+	facebook: {
+		url: "https://facebook.com",
+		label: "Facebook",
+		icon: "facebook",
+	},
+	twitter: {
+		url: "https://twitter.com",
+		label: "Twitter",
+		icon: "twitter",
+	},
+	instagram: {
+		url: "https://instagram.com",
+		label: "Instagram",
+		icon: "instagram",
+	},
+	youtube: {
+		url: "https://youtube.com",
+		label: "YouTube",
+		icon: "youtube",
+	},
+};
+
+export const hours = {
+	monday: "9:00 - 17:00",
+	tuesday: "9:00 - 17:00",
+	wednesday: "9:00 - 17:00",
+	thursday: "9:00 - 17:00",
+	friday: "9:00 - 17:00",
+	saturday: "9:00 - 17:00",
+	sunday: "Closed",
+};
